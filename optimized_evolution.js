0c61aa1787b9977e98c16038a67dcc395265c6d2
GOA-neurons <yewintthetlwin@gmail.com>
2026-01-08 14:26:17 +0630

    final fix neon

--- optimized_evolution.js
+++ optimized_evolution.js
@@ -1,29 +1,15 @@
 const admin = require('firebase-admin');
 const { Client } = require('pg');
-
-if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))
-    });
-}
+admin.initializeApp({ credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)) });
 const db = admin.firestore();
-
-async function startEvolution() {
-    const client = new Client({
-        connectionString: process.env.NEON_DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
-    });
-    try {
-        await client.connect();
-        const snapshot = await db.collection('neurons').limit(10).get();
-        for (const doc of snapshot.docs) {
-            await client.query('INSERT INTO neurons (data) VALUES ($1)', [JSON.stringify(doc.data())]);
-        }
-        console.log('🔥 GOA: Neon Sync Done!');
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await client.end();
-    }
+async function run() {
+  const client = new Client({ connectionString: process.env.NEON_DATABASE_URL, ssl: { rejectUnauthorized: false } });
+  await client.connect();
+  const snap = await db.collection('neurons').limit(1).get();
+  for (const doc of snap.docs) {
+    await client.query('INSERT INTO neurons (data) VALUES ($1)', [JSON.stringify(doc.data())]);
+  }
+  await client.end();
+  console.log('✅ Success!');
 }
-startEvolution();
+run();
