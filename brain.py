2b1c4e2739a3c0f78b3d2e4ed7c2557d2f766fc3
root <root@f1b374936595>
2026-02-26 12:25:11 +0000

    🧬 Gen 6659 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,33 +1,27 @@
 import numpy as np
-import numba
-
-@numba.jit(nopython=True)
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-@numba.jit(nopython=True)
-def sigmoid_derivative(x):
-    return x * (1 - x)
 
 class Brain:
-    def __init__(self, inputs, outputs):
-        self.input_shape = inputs
-        self.output_shape = outputs
-        self.weights = np.random.rand(self.output_shape[0], self.input_shape[0])
-        self.biases = np.zeros((self.output_shape[0],))
+    def __init__(self):
+        self.synapses = {}
 
-    def forward_pass(self, inputs):
-        inputs = np.array(inputs)
-        hidden_layer = sigmoid(np.dot(inputs, self.weights) + self.biases)
-        output = sigmoid(np.dot(hidden_layer, self.weights) + self.biases)
-        return output
+    def connect(self, neuron1, neuron2, weight):
+        if neuron1 not in self.synapses:
+            self.synapses[neuron1] = {}
+        self.synapses[neuron1][neuron2] = weight
 
-    def train(self, inputs, targets):
-        inputs = np.array(inputs)
-        targets = np.array(targets)
-        hidden_layer = sigmoid(np.dot(inputs, self.weights) + self.biases)
-        output = sigmoid(np.dot(hidden_layer, self.weights) + self.biases)
-        error = targets - output
-        delta = error * sigmoid_derivative(output)
-        self.weights += np.dot(delta, hidden_layer.T)
-        self.biases += np.sum(delta, axis=0, keepdims=True)
+    def fire(self, neuron):
+        if neuron not in self.synapses:
+            return np.zeros(())
+        sum = 0
+        for connected_neuron, weight in self.synapses[neuron].items():
+            sum += weight * self.fire(connected_neuron)
+        if sum > 0:
+            return np.ones(() * sum)
+        else:
+            return np.zeros(())
+
+    def process(self, inputs):
+        outputs = {}
+        for neuron, input_value in inputs.items():
+            outputs[neuron] = self.fire(neuron) * input_value
+        return outputs
