a76e86db4ab95ef04ff2818da3051097e60fc26d
root <root@6570c282e0ab>
2026-02-26 14:44:46 +0000

    🧬 Gen 6711 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,39 +1,37 @@
+import numpy as np
+from scipy.optimize import minimize
+
 class Brain:
-    def __init__(self):
-        self.weights = {}
-        self.biases = {}
+    def __init__(self, neurons):
+        self.neurons = neurons
+        self.synaptic_weights = np.random.rand(neurons, neurons)
+        self.synaptic_thresholds = np.random.rand(neurons)
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    def fire(self, input_array):
+        output_array = np.zeros(self.neurons)
+        for i in range(self.neurons):
+            output_array[i] = self.calculate_output(i, input_array)
+        return output_array
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
+    def calculate_output(self, neuron_index, input_array):
+        sum_of_inputs = np.dot(self.synaptic_weights[neuron_index], input_array)
+        output = np.where(sum_of_inputs > self.synaptic_thresholds[neuron_index], 1, 0)
+        return output
 
-    def train(self, inputs, targets):
-        learning_rate = 0.1
-        for epoch in range(1000):
-            for i in range(len(inputs)):
-                input_layer = inputs[i]
-                target = targets[i]
-                predictions = self.forward_pass(input_layer)
-                error = target - predictions
-                self.backward_pass(input_layer, error, learning_rate)
-        return self
+    def learn(self, input_array, target_output):
+        error = np.mean((self.fire(input_array) - target_output) ** 2)
+        gradients = np.zeros((self.neurons, self.neurons))
+        for i in range(self.neurons):
+            for j in range(self.neurons):
+                gradients[i, j] = -2 * (self.fire(input_array)[i] - target_output[i]) * self.fire(input_array)[j]
+        self.synaptic_weights -= 0.1 * gradients
+        return error
 
-    def forward_pass(self, input_layer):
-        layer_outputs = {}
-        layer_outputs[0] = input_layer
-        for i in range(1, len(self.weights) + 1):
-            layer_outputs[i] = np.dot(layer_outputs[i - 1], self.weights[i - 1]) + self.biases[i - 1]
-            layer_outputs[i] = self.sigmoid(layer_outputs[i])
-        return layer_outputs[len(self.weights)]
+    def optimize(self, input_array, target_output):
+        error = np.inf
+        while error > 1e-6:
+            error = self.learn(input_array, target_output)
+        return error
 
-    def backward_pass(self, input_layer, error, learning_rate):
-        layer_outputs = {}
-        layer_outputs[0] = input_layer
-        for i in range(len(self.weights), 0, -1):
-            error = error * self.sigmoid_derivative(layer_outputs[i])
-            self.weights[i - 1] += learning_rate * np.dot(layer_outputs[i - 1].T, error)
-            self.biases[i - 1] += learning_rate * error
-            error = np.dot(error, self.weights[i - 1].T)
-        return self
+    def generate_synaptic_weights(self):
+        return self.synaptic_weights
