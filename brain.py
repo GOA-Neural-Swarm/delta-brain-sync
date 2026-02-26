6633528a644ac778ac2dc666769a3a0eb0c73bf6
root <root@6570c282e0ab>
2026-02-26 19:50:44 +0000

    🧬 Gen 6860 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,37 +1,16 @@
 import numpy as np
+from scipy.optimize import minimize
 
 class Brain:
-    def __init__(self):
-        self.weights = np.random.rand(1, 10)
-        self.biases = np.random.rand(10)
-        self.cache = {}
+    def __init__(self, num_neurons):
+        self.num_neurons = num_neurons
+        self.synaptic_weights = np.random.rand(num_neurons, num_neurons)
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    def think(self, input_array):
+        return np.dot(input_array, self.synaptic_weights)
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-
-    def forward_pass(self, inputs):
-        if tuple(inputs) in self.cache:
-            return self.cache[tuple(inputs)]
-        outputs = np.dot(inputs, self.weights) + self.biases
-        self.cache[tuple(inputs)] = outputs
-        return self.sigmoid(outputs)
-
-    def backward_pass(self, inputs, expected_output):
-        output = self.forward_pass(inputs)
-        output_derivative = self.sigmoid_derivative(output)
-        weight_update = inputs * output_derivative
-        bias_update = output_derivative
-        self.weights += 0.1 * weight_update
-        self.biases += 0.1 * bias_update
-        return output_derivative
-
-    def train(self, inputs, expected_outputs):
-        for inputs, expected_output in zip(inputs, expected_outputs):
-            output_derivative = self.backward_pass(inputs, expected_output)
-            return output_derivative
-
-    def predict(self, inputs):
-        return self.forward_pass(inputs)
+    def learn(self, input_array, output_array):
+        def objective(weights):
+            return np.mean((np.dot(input_array, weights) - output_array) ** 2)
+        result = minimize(objective, self.synaptic_weights.flatten(), method="SLSQP")
+        self.synaptic_weights = result.x.reshape(self.synaptic_weights.shape)
