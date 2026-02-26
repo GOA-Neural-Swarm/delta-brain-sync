53a030828f46d875055e62eddaf6009352e8999c
root <root@6570c282e0ab>
2026-02-26 17:30:52 +0000

    🧬 Gen 6794 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,25 +1,16 @@
 class Brain:
     def __init__(self):
-        self.synapses = {}
         self.neurons = {}
 
-    def connect(self, neuron1, neuron2):
-        if neuron1 not in self.synapses:
-            self.synapses[neuron1] = {}
-        if neuron2 not in self.synapses[neuron1]:
-            self.synapses[neuron1][neuron2] = 0
-        if neuron2 not in self.neurons:
-            self.neurons[neuron2] = 0
+    def add_neuron(self, id, func):
+        self.neurons[id] = func
 
-    def fire(self, neuron):
-        if neuron in self.synapses:
-            for connected_neuron, strength in self.synapses[neuron].items():
-                self.neurons[connected_neuron] += strength
-        if neuron not in self.neurons:
-            self.neurons[neuron] = 0
+    def process(self, inputs):
+        outputs = {}
+        for neuron_id, neuron_func in self.neurons.items():
+            output = neuron_func(inputs)
+            outputs[neuron_id] = output
+        return outputs
 
-    def update(self):
-        for neuron, value in self.neurons.items():
-            if value > 1:
-                self.fire(neuron)
-                self.neurons[neuron] = 0
+    def __call__(self, inputs):
+        return self.process(inputs)
