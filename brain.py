6dd4b6f17915995bcf53cf80aac2cfd423cb5b99
root <root@6570c282e0ab>
2026-02-26 18:13:24 +0000

    🧬 Gen 6814 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,1 +1,35 @@
-import math
+import numpy as np
+
+class Brain:
+    def __init__(self, num_neurons, num_layers):
+        self.num_neurons = num_neurons
+        self.num_layers = num_layers
+        self.weights = [np.random.rand(num_neurons, num_neurons) for _ in range(num_layers - 1)]
+        self.biases = [np.zeros((num_neurons,)) for _ in range(num_layers - 1)]
+
+    def forward_pass(self, inputs):
+        outputs = inputs
+        for i in range(self.num_layers - 1):
+            outputs = np.tanh(np.dot(outputs, self.weights[i]) + self.biases[i])
+        return outputs
+
+    def backward_pass(self, inputs, targets):
+        errors = np.zeros((self.num_layers - 1, self.num_neurons))
+        outputs = inputs
+        for i in range(self.num_layers - 1, 0, -1):
+            errors[i - 1] = np.dot(outputs, errors[i - 1].T)
+            outputs = np.tanh(np.dot(outputs, self.weights[i - 1]) + self.biases[i - 1])
+        return errors
+
+    def update_weights(self, inputs, targets, learning_rate):
+        errors = self.backward_pass(inputs, targets)
+        for i in range(self.num_layers - 1):
+            self.weights[i] += learning_rate * np.dot(outputs.T, errors[i])
+            self.biases[i] += learning_rate * np.sum(errors[i], axis=0, keepdims=True)
+        return
+
+    def train(self, inputs, targets, learning_rate, epochs):
+        for _ in range(epochs):
+            outputs = self.forward_pass(inputs)
+            self.update_weights(inputs, targets, learning_rate)
+        return
