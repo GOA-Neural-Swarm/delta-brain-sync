10bceca58456923236914bf6f964e16eaec85fb0
root <root@2e7b7ab49117>
2026-02-26 14:23:57 +0000

    🧬 Gen 6701 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,39 +1,16 @@
+import numpy as np
+
 class Brain:
     def __init__(self):
-        self.synapses = {}
         self.neurons = {}
 
-    def process_neural_signal(self, signal):
-        # Optimize neural processing by using NumPy arrays for efficient computations
-        import numpy as np
+    def add_neuron(self, id, inputs):
+        self.neurons[id] = {'inputs': inputs, 'output': 0}
 
-        # Initialize neuron activations
-        activations = np.zeros((len(self.neurons),))
+    def fire(self, neuron_id):
+        neuron = self.neurons[neuron_id]
+        output = np.sum([input * weight for input, weight in zip(neuron['inputs'], neuron['inputs'])])
+        neuron['output'] = output
 
-        # Propagate neural signals through synapses
-        for synapse in self.synapses.values():
-            weights, bias = synapse
-            activations += np.dot(weights, np.transpose(signal)) + bias
-
-        # Apply sigmoid function to neuron activations
-        activations = 1 / (1 + np.exp(-activations))
-
-        return activations
-
-    def update_synapses(self, signal):
-        # Update synapses using Hebbian learning
-        for neuron in self.neurons.values():
-            weights = neuron['weights']
-            for synapse in self.synapses.values():
-                weights += np.dot(synapse[0], signal)  # Update weights
-                # Update bias
-                bias = neuron['bias']
-                synapse[1] += np.sum(signal)  # Update bias
-
-    def add_synapse(self, neuron1, neuron2, weight, bias):
-        # Add a new synapse between two neurons
-        self.synapses[(neuron1, neuron2)] = (weight, bias)
-
-    def add_neuron(self, neuron_id, activation_function):
-        # Add a new neuron to the brain
-        self.neurons[neuron_id] = {'activation_function': activation_function}
+    def get_output(self, neuron_id):
+        return self.neurons[neuron_id]['output']
