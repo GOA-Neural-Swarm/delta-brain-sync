6c2de6cd8a9d8895975520a24e93282ce6bc75a7
root <root@6570c282e0ab>
2026-02-26 17:37:24 +0000

    🧬 Gen 6797 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,39 +1,23 @@
-import numpy as np
-from scipy import ndimage
-
 class Brain:
-    def __init__(self, num_inputs, num_hidden, num_outputs):
-        self.num_inputs = num_inputs
-        self.num_hidden = num_hidden
-        self.num_outputs = num_outputs
-        self.weights_ih = np.random.rand(num_inputs, num_hidden)
-        self.weights_ho = np.random.rand(num_hidden, num_outputs)
-        self.biases_ih = np.zeros((1, num_hidden))
-        self.biases_ho = np.zeros((1, num_outputs))
+    def __init__(self):
+        self.neurons = {}
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    def add_neuron(self, neuron_id, weights):
+        self.neurons[neuron_id] = weights
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
+    def process(self, inputs):
+        import numpy as np
+        import numba
+        @numba.njit
+        def compute_neuron_output(neuron_id, inputs):
+            value = np.dot(np.array([weights.get(input_id, 0) for input_id in inputs]), np.array(list(inputs.values())))
+            return value
 
-    def forward_pass(self, inputs):
-        hidden_layer = np.dot(inputs, self.weights_ih) + self.biases_ih
-        hidden_layer = self.sigmoid(hidden_layer)
-        output_layer = np.dot(hidden_layer, self.weights_ho) + self.biases_ho
-        output_layer = self.sigmoid(output_layer)
-        return output_layer
+        for neuron_id, weights in self.neurons.items():
+            self.neurons[neuron_id] = compute_neuron_output(neuron_id, inputs)
 
-    def backward_pass(self, inputs, targets):
-        hidden_layer = self.forward_pass(inputs)
-        output_layer = self.sigmoid_derivative(hidden_layer)
-        output_error = targets - hidden_layer
-        hidden_error = np.dot(output_error, self.weights_ho.T) * self.sigmoid_derivative(hidden_layer)
-        self.weights_ho += np.dot(hidden_layer.T, output_error)
-        self.weights_ih += np.dot(inputs.T, hidden_error)
-        self.biases_ho += output_error
-        self.biases_ih += hidden_error
-
-    def train(self, inputs, targets, epochs):
-        for _ in range(epochs):
-            self.backward_pass(inputs, targets)
+    def get_output(self):
+        outputs = {}
+        for neuron_id, value in self.neurons.items():
+            outputs[neuron_id] = value
+        return outputs
