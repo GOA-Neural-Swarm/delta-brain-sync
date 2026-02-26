139801b0cb27233d9ee5fe5f616321e329ecfbaa
root <root@f1b374936595>
2026-02-26 12:23:15 +0000

    🧬 Gen 6658 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,20 +1,33 @@
+import numpy as np
+import numba
+
+@numba.jit(nopython=True)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+@numba.jit(nopython=True)
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
 class Brain:
-    def __init__(self):
-        self.neurons = [{} for _ in range(10000)]  # Pre-allocate neuron dict array
-        self.connections = [{} for _ in range(10000)]  # Pre-allocate connection dict array
+    def __init__(self, inputs, outputs):
+        self.input_shape = inputs
+        self.output_shape = outputs
+        self.weights = np.random.rand(self.output_shape[0], self.input_shape[0])
+        self.biases = np.zeros((self.output_shape[0],))
 
-    def process(self, input_data):
-        for neuron_id, inputs in enumerate(input_data):
-            if inputs:  # Non-empty input list
-                for input_id, weight in inputs:
-                    self.connections[neuron_id][input_id] += weight
-            if not self.connections[neuron_id]:  # No connections, reset neuron
-                self.connections[neuron_id] = {}
+    def forward_pass(self, inputs):
+        inputs = np.array(inputs)
+        hidden_layer = sigmoid(np.dot(inputs, self.weights) + self.biases)
+        output = sigmoid(np.dot(hidden_layer, self.weights) + self.biases)
+        return output
 
-    def update(self):
-        for neuron_id, connections in enumerate(self.connections):
-            if connections:
-                total_weight = sum(connections.values())
-                self.neurons[neuron_id] = total_weight  # Update neuron state
-            else:
-                self.neurons[neuron_id] = 0  # Reset neuron state
+    def train(self, inputs, targets):
+        inputs = np.array(inputs)
+        targets = np.array(targets)
+        hidden_layer = sigmoid(np.dot(inputs, self.weights) + self.biases)
+        output = sigmoid(np.dot(hidden_layer, self.weights) + self.biases)
+        error = targets - output
+        delta = error * sigmoid_derivative(output)
+        self.weights += np.dot(delta, hidden_layer.T)
+        self.biases += np.sum(delta, axis=0, keepdims=True)
