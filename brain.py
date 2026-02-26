ebcb0ce8807f0229f7bc915c727f9391e40046b2
root <root@6570c282e0ab>
2026-02-26 16:25:25 +0000

    🧬 Gen 6761 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,36 +1,29 @@
 import numpy as np
 
 class Brain:
-    def __init__(self, inputs, outputs, layers):
-        self.inputs = inputs
-        self.outputs = outputs
-        self.layers = [np.zeros((i, o)) for i, o in zip([inputs]+[l[1] for l in layers]+[outputs], [outputs]+[l[2] for l in layers]+[inputs])]
-        self.weights = [[np.random.rand(i, o) for o in [l[1] for l in layers]+[outputs]] for i in [inputs]+[l[1] for l in layers]+[outputs]]
-        self.biases = [[np.zeros((i, 1)) for i in [l[1] for l in layers]+[outputs]] for _ in range(len(layers)+1)]
+    def __init__(self):
+        self.synapses = {}
 
-    def sigmoid(self, x):
-        return 1/(1+np.exp(-x))
+    def learn(self, inputs, outputs):
+        self.synapses = {i: {} for i in range(len(inputs))}
+        for i in range(len(inputs)):
+            for j in range(len(outputs)):
+                if inputs[i] not in self.synapses[i]:
+                    self.synapses[i][inputs[i]] = np.random.rand()
+                self.synapses[i][inputs[i]] += outputs[j] * inputs[i]
 
-    def sigmoid_derivative(self, x):
-        return x*(1-x)
+    def predict(self, inputs):
+        outputs = []
+        for i in range(len(inputs)):
+            total = 0
+            for j in range(len(inputs)):
+                if inputs[j] in self.synapses[i]:
+                    total += self.synapses[i][inputs[j]]
+            outputs.append(total)
+        return outputs
 
-    def train(self, inputs, outputs, iterations):
-        for _ in range(iterations):
-            layer_inputs = [np.append(self.layers[0], inputs)]
-            for i, (in_layer, out_layer, _) in enumerate(self.layers[1:]):
-                layer_inputs.append(np.dot(in_layer, self.weights[i+1].T)+self.biases[i+1])
-                layer_inputs[i+1] = self.sigmoid(layer_inputs[i+1])
-            error = np.sum((layer_inputs[-1]-outputs)**2)
-            dweights = [np.dot(layer_inputs[i+1].T, layer_inputs[i+1]-outputs)]
-            for i, (in_layer, out_layer, _) in enumerate(self.layers[:-1][::-1]):
-                dweights[i] = np.dot(dweights[i+1], out_layer) + dweights[i]
-                layer_inputs[i+1] = self.sigmoid_derivative(layer_inputs[i+1])
-            self.weights = [w - 0.1 * dw for w, dw in zip(self.weights, dweights)]
-            self.biases = [b - 0.1 * np.sum((dw > 0) * (dw < 1), axis=0, keepdims=True) for b, dw in zip(self.biases, dweights)]
-
-    def think(self, inputs):
-        layer_inputs = [np.append(self.layers[0], inputs)]
-        for i, (in_layer, out_layer, _) in enumerate(self.layers[1:]):
-            layer_inputs.append(np.dot(in_layer, self.weights[i+1].T)+self.biases[i+1])
-            layer_inputs[i+1] = self.sigmoid(layer_inputs[i+1])
-        return layer_inputs[-1]
+    def update(self, inputs, outputs):
+        for i in range(len(inputs)):
+            for j in range(len(outputs)):
+                if inputs[i] in self.synapses[i]:
+                    self.synapses[i][inputs[i]] += (outputs[j] - self.synapses[i][inputs[i]]) * inputs[i]
