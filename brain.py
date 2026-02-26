004f88e48f360d9910dfcdbe80b090f7ed86ba06
root <root@6570c282e0ab>
2026-02-26 17:09:43 +0000

    🧬 Gen 6783 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,27 +1,12 @@
-import numpy as np
+import joblib
+import multiprocessing
 
 class Brain:
-    def __init__(self, layers):
-        self.layers = layers
-        self.weights = [np.random.rand(layers[i+1], layers[i]) for i in range(len(layers)-1)]
-        self.biases = [np.random.rand(layers[i+1], 1) for i in range(len(layers)-1)]
+    def __init__(self):
+        self.neurons = []
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    def add_neuron(self, neuron):
+        self.neurons.append(neuron)
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-
-    def forward_pass(self, inputs):
-        outputs = inputs
-        for i in range(len(self.layers)-1):
-            outputs = self.sigmoid(np.dot(self.weights[i], outputs) + self.biases[i])
-        return outputs
-
-    def backpropagate(self, inputs, targets):
-        outputs = self.forward_pass(inputs)
-        errors = targets - outputs
-        for i in range(len(self.layers)-1, 0, -1):
-            errors = errors * self.sigmoid_derivative(outputs)
-            outputs = np.dot(self.weights[i-1].T, errors)
-        return errors
+    def process(self):
+        return joblib.Parallel(n_jobs=multiprocessing.cpu_count())(lambda neuron: neuron.process() for neuron in self.neurons)
