57a0a6cbfec3dc65fb6157ca90473549e93f14c6
root <root@dbcdde785a8d>
2026-02-26 11:28:40 +0000

    🧬 Gen 6647 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,29 +1,9 @@
-import numpy as np
-from scipy import optimize
 
-class Brain:
-    def __init__(self):
-        self.weights = np.random.rand()
-        self.bias = 0
-
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-
-    def train(self, inputs, targets):
-        learning_rate = 0.01
-        for _ in range(1000):
-            outputs = self.forward_pass(inputs)
-            error = targets - outputs
-            self.weights += learning_rate * np.dot(error, inputs)
-            self.bias += learning_rate * np.sum(error)
-
-    def forward_pass(self, inputs):
-        return self.sigmoid(np.dot(inputs, self.weights) + self.bias)
-
-    def backward_pass(self, inputs, targets):
-        outputs = self.forward_pass(inputs)
-        error = targets - outputs
-        return error * self.sigmoid_derivative(outputs)
+brain = Brain()
+neuron1 = Neuron(1)
+neuron2 = Neuron(2)
+synapse = Synapse(neuron1, neuron2)
+brain.add_neuron(neuron1)
+brain.add_neuron(neuron2)
+brain.add_synapse(synapse)
+brain.process()
