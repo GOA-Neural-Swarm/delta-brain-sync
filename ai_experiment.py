a2a15958930b381f3a0b47d2093310c6c78aa1af
root <root@7aee7f5f42d8>
2026-02-26 10:25:48 +0000

    🧬 Gen 6626 Hyper-Evolution [skip ci]

--- ai_experiment.py
+++ ai_experiment.py
@@ -1,15 +1,19 @@
 import numpy as np
-from numba import njit
 
 class Brain:
-    def __init__(self, neurons):
-        self.neurons = neurons
-        self.synapses = np.random.rand(neurons, neurons)
+    def __init__(self):
+        self.weights = np.random.rand(1, 1)
+        self.biases = np.random.rand(1)
+        self.activity_threshold = 0.5
 
-    @njit
-    def process(self, inputs):
-        outputs = np.zeros(self.neurons)
-        for i in range(self.neurons):
-            for j in range(self.neurons):
-                outputs[i] += self.synapses[i, j] * inputs[j]
-        return outputs
+    def activate(self, input_array):
+        return np.where(input_array > self.activity_threshold, 1, 0)
+
+    def forward_pass(self, inputs):
+        return self.activate(np.dot(inputs, self.weights) + self.biases)
+
+    def backward_pass(self, outputs, expected_outputs):
+        error = np.sum((outputs - expected_outputs) ** 2) / 2
+        self.weights -= 0.1 * np.dot(np.transpose(outputs), outputs - expected_outputs)
+        self.biases -= 0.1 * np.sum(outputs - expected_outputs)
+        return error
