81af2ddf5a280674fa6a6352379cc560afd9b8e4
root <root@6570c282e0ab>
2026-02-26 18:46:18 +0000

    🧬 Gen 6829 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,29 +1,13 @@
+import time
+import numpy as np
+
 class Brain:
-    def __init__(self):
-        self.neurons = {}
-        self.synapses = {}
+    def __init__(self, num_neurons, num_synapses):
+        self.num_neurons = num_neurons
+        self.num_synapses = num_synapses
+        self.neurons = np.zeros((num_neurons, 1), dtype=float)
+        self.synapses = np.zeros((num_synapses, num_neurons), dtype=float)
 
     def process(self, input_data):
-        for neuron_id, weights in self.synapses.items():
-            output = 0
-            for input_id, weight in weights.items():
-                output += self.neurons[input_id] * weight
-            self.neurons[neuron_id] = sigmoid(output)
+        self.neurons = np.dot(self.synapses.T, input_data)
         return self.neurons
-
-    def learn(self, input_data, target_output):
-        for neuron_id, weights in self.synapses.items():
-            output = 0
-            for input_id, weight in weights.items():
-                output += self.neurons[input_id] * weight
-            error = target_output - self.neurons[neuron_id]
-            for input_id, weight in weights.items():
-                self.synapses[neuron_id][input_id] += 0.1 * error * self.neurons[input_id]
-            self.neurons[neuron_id] = sigmoid(output)
-
-    def sigmoid(self, x):
-        return 1 / (1 + math.exp(-x))
-
-    def initialize(self, num_neurons, num_synapses):
-        self.neurons = {i: random.random() for i in range(num_neurons)}
-        self.synapses = {i: {j: random.random() for j in range(num_synapses)} for i in range(num_synapses)}
