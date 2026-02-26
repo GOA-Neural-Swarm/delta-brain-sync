de084065c823013b0d71597ba9910d0a44261e08
root <root@f1b374936595>
2026-02-26 12:34:52 +0000

    🧬 Gen 6664 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -2,28 +2,26 @@ import numpy as np
 
 class Brain:
     def __init__(self):
-        self.weights = np.random.rand(10, 10)
-        self.biases = np.random.rand(10)
+        self.synapses = {}
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    def connect(self, neuron1, neuron2, weight):
+        if neuron1 not in self.synapses:
+            self.synapses[neuron1] = {}
+        self.synapses[neuron1][neuron2] = weight
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
+    def fire(self, neuron):
+        if neuron not in self.synapses:
+            return np.zeros(())
+        sum = 0
+        for connected_neuron, weight in self.synapses[neuron].items():
+            sum += weight * self.fire(connected_neuron)
+        if sum > 0:
+            return np.ones(() * sum)
+        else:
+            return np.zeros(())
 
-    def train(self, inputs, targets):
-        inputs = np.array(inputs)
-        targets = np.array(targets)
-
-        for i in range(1000):
-            hidden_layer = np.dot(inputs, self.weights) + self.biases
-            outputs = self.sigmoid(hidden_layer)
-
-            error = targets - outputs
-            self.weights += np.dot(inputs.T, error * self.sigmoid_derivative(outputs))
-            self.biases += error.mean()
-
-    def think(self, inputs):
-        inputs = np.array(inputs)
-        hidden_layer = np.dot(inputs, self.weights) + self.biases
-        return self.sigmoid(hidden_layer)
+    def process(self, inputs):
+        outputs = {}
+        for neuron, input_value in inputs.items():
+            outputs[neuron] = self.fire(neuron) * input_value
+        return outputs
