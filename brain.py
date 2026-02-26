a6e66b94907aa253f08b58ebdd5734a3be888ba6
root <root@2e7b7ab49117>
2026-02-26 14:53:24 +0000

    🧬 Gen 6718 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,25 +1,23 @@
+import numpy as np
+
 class Brain:
-    def __init__(self, neurons, synapses):
-        self.neurons = neurons
-        self.synapses = synapses
-        self.deltas = [0] * len(neurons)
+    def __init__(self):
+        self.layers = []
+        self.weights = []
+        self.biases = []
+        self.activations = []
 
-    def propagate(self, inputs):
-        for i, neuron in enumerate(self.neurons):
-            neuron['output'] = neuron.get('threshold', 0)  # cache threshold
-            for j, synapse in enumerate(self.synapses[i]):
-                neuron['output'] += synapse['weight'] * inputs[j]
-            if neuron['output'] > neuron.get('threshold', 0):
-                neuron['output'] = 1
-            else:
-                neuron['output'] = 0
+    def add_layer(self, neurons, activation='sigmoid'):
+        self.layers.append(neurons)
+        self.weights.append(np.random.rand(neurons, self.layers[-2] if self.layers else 0))
+        self.biases.append(np.zeros((neurons, 1)))
+        self.activations.append(activation)
 
-    def update(self, targets):
-        for i, neuron in enumerate(self.neurons):
-            error = targets[i] - neuron['output']
-            self.deltas[i] = error
-            for j, synapse in enumerate(self.synapses[i]):
-                synapse['weight'] += error * synapse['learning_rate'] * neuron['output']
-
-    def __repr__(self):
-        return f'Brain(neurons={len(self.neurons)}, synapses={len(self.synapses)})'
+    def forward_pass(self, inputs):
+        inputs = np.array(inputs).reshape((1, -1))
+        for i, (layer, weights, biases, activation) in enumerate(zip(self.layers, self.weights, self.biases, self.activations)):
+            if activation =='sigmoid':
+                inputs = np.where(inputs > 0.5, 1, 0)
+            elif activation =='relu':
+                inputs = np.where(inputs > 0, inputs, 0)
+        return inputs[0]
