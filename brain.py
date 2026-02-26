3cbd298cf6e8ef6f8f18dd64d9dfbbe881d7e95f
root <root@6570c282e0ab>
2026-02-26 16:57:46 +0000

    🧬 Gen 6777 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,11 +1,25 @@
-import numba
-@numba.jit
-def think(self, input_vector):
-    return np.dot(self.synaptic_weights, input_vector)
+import numpy as np
 
-@numba.jit
-def learn(self, input_vector, target_output):
-    output_vector = self.think(input_vector)
-    error = target_output - output_vector
-    self.synaptic_weights[:] += 0.1 * error * input_vector
-    return output_vector
+class Brain:
+    def __init__(self):
+        self.weights = np.random.rand(1000, 1000)
+        self.biases = np.random.rand(1000, 1)
+        self.output = np.zeros((1000, 1))
+
+    def forward_pass(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        return self.output
+
+    def backpropagation(self, inputs, targets):
+        output = self.forward_pass(inputs)
+        error = np.sum((output - targets) ** 2) / 2
+        delta_output = (output - targets)
+        delta_weights = np.dot(delta_output.T, inputs)
+        delta_biases = np.sum(delta_output, axis=0, keepdims=True)
+        return error, delta_weights, delta_biases
+
+    def train(self, inputs, targets, learning_rate=0.01):
+        error, delta_weights, delta_biases = self.backpropagation(inputs, targets)
+        self.weights -= learning_rate * delta_weights
+        self.biases -= learning_rate * delta_biases
+        return error
