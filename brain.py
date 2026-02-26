9e5e5ba6e10ca3fda4e1a18ae52d6edd69d1e592
root <root@dbcdde785a8d>
2026-02-26 11:26:44 +0000

    🧬 Gen 6646 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,37 +1,29 @@
 import numpy as np
-import numba
-
-@numba.jit(nopython=True)
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+from scipy import optimize
 
 class Brain:
-    def __init__(self, num_inputs, num_hidden, num_outputs):
-        self.num_inputs = num_inputs
-        self.num_hidden = num_hidden
-        self.num_outputs = num_outputs
-        self.weights_ih = np.random.rand(num_inputs, num_hidden)
-        self.weights_ho = np.random.rand(num_hidden, num_outputs)
-        self.biases_ih = np.zeros((num_hidden,))
-        self.biases_ho = np.zeros((num_outputs,))
+    def __init__(self):
+        self.weights = np.random.rand()
+        self.bias = 0
 
-    def feedforward(self, inputs):
-        hidden_layer = np.dot(inputs, self.weights_ih) + self.biases_ih
-        hidden_layer = sigmoid(hidden_layer)
-        outputs = np.dot(hidden_layer, self.weights_ho) + self.biases_ho
-        outputs = sigmoid(outputs)
-        return outputs
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-    def train(self, inputs, targets, learning_rate, num_iterations):
-        for _ in range(num_iterations):
-            hidden_layer = np.dot(inputs, self.weights_ih) + self.biases_ih
-            hidden_layer = sigmoid(hidden_layer)
-            outputs = np.dot(hidden_layer, self.weights_ho) + self.biases_ho
-            outputs = sigmoid(outputs)
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def train(self, inputs, targets):
+        learning_rate = 0.01
+        for _ in range(1000):
+            outputs = self.forward_pass(inputs)
             error = targets - outputs
-            deltas = error * outputs * (1 - outputs)
-            self.weights_ho += np.dot(hidden_layer.T, deltas) * learning_rate
-            self.biases_ho += np.sum(deltas, axis=0, keepdims=True) * learning_rate
-            error = np.dot(deltas, self.weights_ho.T) * hidden_layer * (1 - hidden_layer)
-            self.weights_ih += np.dot(inputs.T, error) * learning_rate
-            self.biases_ih += np.sum(error, axis=0, keepdims=True) * learning_rate
+            self.weights += learning_rate * np.dot(error, inputs)
+            self.bias += learning_rate * np.sum(error)
+
+    def forward_pass(self, inputs):
+        return self.sigmoid(np.dot(inputs, self.weights) + self.bias)
+
+    def backward_pass(self, inputs, targets):
+        outputs = self.forward_pass(inputs)
+        error = targets - outputs
+        return error * self.sigmoid_derivative(outputs)
