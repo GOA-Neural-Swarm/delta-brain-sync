f32c332e38ffc81a4c829900778c73bc52883f82
root <root@6570c282e0ab>
2026-02-26 18:52:32 +0000

    🧬 Gen 6832 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,13 +1,4 @@
-import time
-import numpy as np
+import timeit
 
-class Brain:
-    def __init__(self, num_neurons, num_synapses):
-        self.num_neurons = num_neurons
-        self.num_synapses = num_synapses
-        self.neurons = np.zeros((num_neurons, 1), dtype=float)
-        self.synapses = np.zeros((num_synapses, num_neurons), dtype=float)
-
-    def process(self, input_data):
-        self.neurons = np.dot(self.synapses.T, input_data)
-        return self.neurons
+print(timeit.timeit(lambda: brain.process(), number=1000))
+print(timeit.timeit(lambda: brain.optimize_process(), number=1000))
