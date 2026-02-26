d6c9090a1f66ac6034350060241e7be43828d0d9
root <root@2e7b7ab49117>
2026-02-26 14:48:50 +0000

    🧬 Gen 6715 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,23 +1,18 @@
-# TARGET: brain.py
-
 import numpy as np
 
 class Brain:
-    def __init__(self):
-        self.weights = np.random.rand(100, 100)
-        self.biases = np.random.rand(100)
+    def __init__(self, num_neurons, num_synapses):
+        self.num_neurons = num_neurons
+        self.num_synapses = num_synapses
+        self.neurons = np.zeros((num_neurons, num_synapses))
+        self.synapses = np.random.rand(num_synapses)
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    def fire(self, inputs):
+        outputs = np.zeros(self.num_neurons)
+        for i in range(self.num_neurons):
+            outputs[i] = np.sum(self.synapses * self.neurons[i])
+        return outputs
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-
-    def train(self, inputs, outputs):
-        learning_rate = 0.01
-        for i in range(1000):
-            for j in range(len(inputs)):
-                layer_1 = self.sigmoid(np.dot(inputs[j], self.weights) + self.biases)
-                layer_2 = self.sigmoid(np.dot(layer_1, self.weights) + self.biases)
-                layer_2_error = outputs[j] - layer_2
-                layer_2_delta = layer_2_error
+    def learn(self, inputs, outputs):
+        for i in range(self.num_neurons):
+            self.synapses[i] += np.dot(self.neurons[i], outputs[i]) * 0.1
