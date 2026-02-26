d10fa4ff39c6c79b6c359e54b67a0150b7253553
root <root@2e7b7ab49117>
2026-02-26 14:30:15 +0000

    🧬 Gen 6705 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,10 +1,13 @@
-from numba import njit
 import numpy as np
 
-@njit
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+class Brain:
+    def __init__(self, num_neurons, num_synapses):
+        self.neurons = np.random.rand(num_neurons)
+        self.synapses = np.random.rand(num_synapses)
+        self.weights = np.random.rand(num_neurons, num_synapses)
 
-@njit
-def sigmoid_derivative(x):
-    return x * (1 - x)
+    def fire(self, input_signals):
+        outputs = np.dot(input_signals, self.weights)
+        self.neurons += outputs
+        self.synapses *= self.synapses
+        return self.neurons
