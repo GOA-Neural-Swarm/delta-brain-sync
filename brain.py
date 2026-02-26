9942716468cdf1fe7af5aef50a90d40f592c00b7
root <root@6570c282e0ab>
2026-02-26 20:04:17 +0000

    🧬 Gen 6867 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,23 +1,51 @@
-class Brain:
-    def __init__(self):
-        self.synapses = {}
-        self.neurons = []
+import random
+from brain import Brain, NeuralNetwork, Sigmoid, ReLU
 
-    def create_synapse(self, neuron1, neuron2):
-        if (neuron1, neuron2) in self.synapses:
-            return
-        self.synapses[(neuron1, neuron2)] = 0
-        self.synapses[(neuron2, neuron1)] = 0
+# Create brain
+brain = Brain()
 
-    def fire_synapse(self, neuron1, neuron2):
-        if (neuron1, neuron2) not in self.synapses:
-            return
-        self.synapses[(neuron1, neuron2)] += 1
-        if self.synapses[(neuron1, neuron2)] >= 10:
-            self.synapses[(neuron1, neuron2)] = 0
+# Set number of inputs, outputs, and hidden layers
+num_inputs = 3
+num_outputs = 2
+num_hidden_layers = 2
 
-    def think(self):
-        for neuron in self.neurons:
-            for other in self.neurons:
-                if neuron!= other:
-                    self.fire_synapse(neuron, other)
+# Create neural network
+neural_network = NeuralNetwork(num_inputs, num_outputs, num_hidden_layers)
+
+# Set weights and biases
+for layer in range(num_hidden_layers + 1):
+    if layer == 0:
+        num_neurons = num_inputs
+    else:
+        num_neurons = num_outputs
+
+    for neuron in range(num_neurons):
+        weights = [random.random() for _ in range(num_inputs)]
+        biases = [random.random() for _ in range(num_neurons)]
+
+        neural_network.weights[neuron] = weights
+        neural_network.biases[neuron] = biases
+
+# Set sigmoid and ReLU functions
+sigmoid = Sigmoid()
+relu = ReLU()
+
+# Set activation functions
+activation_functions = [sigmoid if layer < num_hidden_layers else relu for layer in range(num_hidden_layers + 1)]
+
+# Set neural network processing
+def process(self, input_data: np.ndarray) -> np.ndarray:
+    output_data = input_data
+    for layer in range(num_hidden_layers + 1):
+        for neuron in range(num_neurons):
+            if layer == 0:
+                output_data[neuron] = np.sum([weight * input_data[j] for j, weight in enumerate(self.weights[neuron])]) + self.biases[neuron]
+            else:
+                output_data[neuron] = activation_functions[layer](np.sum([weight * output_data[j] for j, weight in enumerate(self.weights[neuron])]) + self.biases[neuron])
+
+    return output_data
+
+# Process input data
+output_data = brain.process(input_data)
+
+print(output_data)
