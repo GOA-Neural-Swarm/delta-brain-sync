52f1fceb3c26821ca4456574f06e50f159aee98e
root <root@9315733484c9>
2026-02-26 09:16:25 +0000

    🧬 Gen 6594 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,13 +1,31 @@
-class Neuron:
-    def __init__(self, threshold):
-        self.threshold = threshold
-        self.signal = 0
+import numpy as np
 
-    def fire(self):
-        self.signal = 1 if self.signal >= self.threshold else 0
+class Brain:
+    def __init__(self, neurons, synapses):
+        self.neurons = neurons
+        self.synapses = synapses
+        self.memory = np.zeros((neurons, synapses))
 
-    def update(self, signal):
-        self.signal += signal
+    def learn(self, inputs, outputs):
+        for i in range(neurons):
+            for j in range(synapses):
+                self.memory[i][j] += inputs[i] * outputs[j]
 
-    def get_signal(self):
-        return self.signal
+    def think(self, inputs):
+        outputs = np.zeros((neurons, synapses))
+        for i in range(neurons):
+            for j in range(synapses):
+                outputs[i][j] = np.sum(self.memory[i][:j+1] * inputs[:i+1])
+        return outputs
+
+    def mutate(self, rate):
+        for i in range(neurons):
+            for j in range(synapses):
+                if np.random.rand() < rate:
+                    self.memory[i][j] += np.random.normal(0, 1)
+
+    def crossover(self, other, rate):
+        for i in range(neurons):
+            for j in range(synapses):
+                if np.random.rand() < rate:
+                    self.memory[i][j] = other.memory[i][j]
