fc6e511c0d4225c9f4ac8d61fc09b912784b3b5f
root <root@6570c282e0ab>
2026-02-26 14:57:07 +0000

    🧬 Gen 6717 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,29 +1,25 @@
-import numpy as np
-
 class Brain:
-    def __init__(self, num_inputs, num_hidden, num_outputs):
-        self.weights_i_h = np.random.rand(num_inputs, num_hidden)
-        self.weights_h_o = np.random.rand(num_hidden, num_outputs)
-        self.biases_i_h = np.zeros((1, num_hidden))
-        self.biases_h_o = np.zeros((1, num_outputs))
+    def __init__(self, neurons, synapses):
+        self.neurons = neurons
+        self.synapses = synapses
+        self.deltas = [0] * len(neurons)
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+    def propagate(self, inputs):
+        for i, neuron in enumerate(self.neurons):
+            neuron['output'] = neuron.get('threshold', 0)  # cache threshold
+            for j, synapse in enumerate(self.synapses[i]):
+                neuron['output'] += synapse['weight'] * inputs[j]
+            if neuron['output'] > neuron.get('threshold', 0):
+                neuron['output'] = 1
+            else:
+                neuron['output'] = 0
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
+    def update(self, targets):
+        for i, neuron in enumerate(self.neurons):
+            error = targets[i] - neuron['output']
+            self.deltas[i] = error
+            for j, synapse in enumerate(self.synapses[i]):
+                synapse['weight'] += error * synapse['learning_rate'] * neuron['output']
 
-    def train(self, inputs, targets):
-        outputs = self.predict(inputs)
-        error = targets - outputs
-        self.weights_i_h += np.dot(inputs.T, error * self.sigmoid_derivative(outputs))
-        self.weights_h_o += np.dot(error * self.sigmoid_derivative(outputs).reshape(-1, 1), outputs.reshape(1, -1))
-        self.biases_i_h += error * self.sigmoid_derivative(outputs)
-        self.biases_h_o += error * self.sigmoid_derivative(outputs)
-
-    def predict(self, inputs):
-        hidden_layer = np.dot(inputs, self.weights_i_h) + self.biases_i_h
-        hidden_layer = self.sigmoid(hidden_layer)
-        output_layer = np.dot(hidden_layer, self.weights_h_o) + self.biases_h_o
-        output_layer = self.sigmoid(output_layer)
-        return output_layer
+    def __repr__(self):
+        return f'Brain(neurons={len(self.neurons)}, synapses={len(self.synapses)})'
