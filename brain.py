1de405fb45c7c72cb580afc3345d33dccdd002e5
root <root@6570c282e0ab>
2026-02-26 15:43:06 +0000

    🧬 Gen 6740 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,22 +1,27 @@
 import numpy as np
 
 class Brain:
-    def __init__(self, neurons):
-        self.neurons = neurons
-        self.weights = np.random.rand(neurons, neurons)
-        self.biases = np.random.rand(neurons)
+    def __init__(self):
+        self.weights = np.random.rand(1000, 1000)
+        self.biases = np.random.rand(1000)
+        self.input_neurons = np.zeros(1000)
+        self.output_neurons = np.zeros(1000)
 
-    def forward_pass(self, inputs):
-        outputs = np.dot(inputs, self.weights) + self.biases
-        return outputs
+    def forward_propagate(self, inputs):
+        self.input_neurons = inputs
+        self.output_neurons = np.dot(self.input_neurons, self.weights.T) + self.biases
+        return self.output_neurons
 
-    def backward_pass(self, errors):
-        weights_update = np.dot(errors, self.neurons)
-        biases_update = errors
-        return weights_update, biases_update
+    def backward_propagate(self, targets, learning_rate=0.01):
+        output_errors = targets - self.output_neurons
+        weight_deltas = np.dot(output_errors.T, self.input_neurons)
+        bias_deltas = output_errors
+        self.weights -= learning_rate * weight_deltas
+        self.biases -= learning_rate * bias_deltas
+        return weight_deltas, bias_deltas
 
-    def optimize(self, inputs, outputs):
-        errors = np.abs(outputs - self.forward_pass(inputs))
-        weights_update, biases_update = self.backward_pass(errors)
-        self.weights -= 0.01 * weights_update
-        self.biases -= 0.01 * biases_update
+    def train(self, inputs, targets, learning_rate=0.01, iterations=1000):
+        for _ in range(iterations):
+            self.forward_propagate(inputs)
+            weight_deltas, bias_deltas = self.backward_propagate(targets, learning_rate)
+        return self.output_neurons
