b87abd80ac9ccfd6d206f0b126347b3b23acc494
root <root@2e7b7ab49117>
2026-02-26 14:27:41 +0000

    🧬 Gen 6703 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,16 +1,19 @@
-import numpy as np
-
 class Brain:
     def __init__(self):
         self.neurons = {}
+        self.synapses = {}
 
-    def add_neuron(self, id, inputs):
-        self.neurons[id] = {'inputs': inputs, 'output': 0}
+    def add_neuron(self, neuron_id):
+        self.neurons[neuron_id] = {'activation': 0, 'weight': 0}
 
-    def fire(self, neuron_id):
-        neuron = self.neurons[neuron_id]
-        output = np.sum([input * weight for input, weight in zip(neuron['inputs'], neuron['inputs'])])
-        neuron['output'] = output
+    def add_synapse(self, neuron_id1, neuron_id2, weight):
+        if neuron_id1 not in self.synapses:
+            self.synapses[neuron_id1] = {}
+        if neuron_id2 not in self.synapses:
+            self.synapses[neuron_id2] = {}
+        self.synapses[neuron_id1][neuron_id2] = weight
 
-    def get_output(self, neuron_id):
-        return self.neurons[neuron_id]['output']
+    def process(self):
+        for neuron_id, neuron_data in self.neurons.items():
+            neuron_data['activation'] = sum(self.synapses.get(neuron_id, {}).get(neuron_id2, 0) * neuron_data['activation'] for neuron_id2 in self.synapses.get(neuron_id, {}))
+            self.neurons[neuron_id]['activation'] %= 1
