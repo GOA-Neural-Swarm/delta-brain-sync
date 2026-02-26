d9a3e07bdb0bfd5372061e489bc26ce8d1f402af
root <root@6570c282e0ab>
2026-02-26 17:03:43 +0000

    🧬 Gen 6780 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,10 +1,26 @@
 import numpy as np
-import numba
 
-@numba.jit(nopython=True)
 class Brain:
     def __init__(self):
-        self.synaptic_weights = np.random.rand(10, 10)
+        self.synapses = {}
 
-    def think(self, inputs):
-        return np.dot(inputs, self.synaptic_weights)
+    def connect(self, neuron1, neuron2):
+        if (neuron1, neuron2) in self.synapses:
+            self.synapses[(neuron1, neuron2)]['weight'] += 1
+        else:
+            self.synapses[(neuron1, neuron2)] = {'weight': 1}
+
+    def fire(self, neuron):
+        fired_synapses = []
+        for synapse in self.synapses:
+            if synapse[0] == neuron:
+                fired_synapses.append(synapse)
+        for synapse in fired_synapses:
+            self.connect(synapse[0], synapse[1])
+        return np.sum([synapse['weight'] for synapse in fired_synapses])
+
+    def run(self, neurons):
+        results = []
+        for neuron in neurons:
+            results.append(self.fire(neuron))
+        return results
