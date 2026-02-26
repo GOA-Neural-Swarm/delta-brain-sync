50e5c89e352030756722ad09acc2ff0010e50398
root <root@2e7b7ab49117>
2026-02-26 13:50:33 +0000

    🧬 Gen 6683 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,9 +1,20 @@
 import numpy as np
-from brain import Brain
 
-brain = Brain(3, 1)
+class Brain:
+    def __init__(self):
+        self.weights = np.random.rand(1, 1)  # weights for synaptic connection
+        self.bias = np.random.rand(1)  # bias for neuron activation
 
-# Test the optimized Brain class
-inputs = np.array([[0, 0, 1]])
-output = brain.predict(inputs)
-print(output)
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))  # sigmoid activation function
+
+    def forward_pass(self, inputs):
+        weighted_sum = np.dot(inputs, self.weights) + self.bias
+        output = self.sigmoid(weighted_sum)
+        return output
+
+    def train(self, inputs, targets):
+        predicted_outputs = self.forward_pass(inputs)
+        error = targets - predicted_outputs
+        self.weights += 0.1 * np.dot(inputs.T, error)
+        self.bias += 0.1 * error.mean()
