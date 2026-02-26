b1375852378fc9eb4483921c208d41763b9e6b99
root <root@6570c282e0ab>
2026-02-26 19:16:27 +0000

    🧬 Gen 6843 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,11 +1,17 @@
-import numpy as np
-from typing import List
+import concurrent.futures
+import cache
 
 class Brain:
-    def __init__(self, num_neurons: int):
-        self.neurons: List[np.ndarray] = [np.zeros((num_neurons,)) for _ in range(num_neurons)]
+    def __init__(self):
+        self.neurons = []
+        self.cache = cache.Cache()
 
-    def process(self, inputs: List[float]) -> List[float]:
-        for i, input_value in enumerate(inputs):
-            self.neurons[i] += input_value
-        return [np.tanh(neuron) for neuron in self.neurons]
+    def add_neuron(self, neuron):
+        self.neurons.append(neuron)
+        self.cache.add(neuron)
+
+    def process(self):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(neuron.fire) for neuron in self.neurons]
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
