91a1303d397e56d961fdedd8ef3969788d94f16c
root <root@6570c282e0ab>
2026-02-26 15:35:11 +0000

    🧬 Gen 6736 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,1 +1,5 @@
-# TARGET: brain.py
+if __name__ == '__main__':
+    brain = Brain([[2], [3]])
+    inputs = np.array([[1, 2, 3, 4]])
+    targets = np.array([[1, 2]])
+    brain.train(inputs, targets, 0.1)
