0e74205462ea86aa2e4d45800c0bc7778293ffcd
root <root@7aee7f5f42d8>
2026-02-26 10:27:59 +0000

    🧬 Gen 6627 Hyper-Evolution [skip ci]

--- ai_experiment.py
+++ ai_experiment.py
@@ -1,19 +1,7 @@
-import numpy as np
+def sigmoid_derivative(x):
+    return x * (1 - x)
 
-class Brain:
-    def __init__(self):
-        self.weights = np.random.rand(1, 1)
-        self.biases = np.random.rand(1)
-        self.activity_threshold = 0.5
-
-    def activate(self, input_array):
-        return np.where(input_array > self.activity_threshold, 1, 0)
-
-    def forward_pass(self, inputs):
-        return self.activate(np.dot(inputs, self.weights) + self.biases)
-
-    def backward_pass(self, outputs, expected_outputs):
-        error = np.sum((outputs - expected_outputs) ** 2) / 2
-        self.weights -= 0.1 * np.dot(np.transpose(outputs), outputs - expected_outputs)
-        self.biases -= 0.1 * np.sum(outputs - expected_outputs)
-        return error
+def dReLU(x):
+    x[x < 0] = 0
+    x[x > 0] = 1
+    return x
