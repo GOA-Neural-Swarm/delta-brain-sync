85dc0b2a9f233e1d104b173035cf234b00fea994
root <root@2e7b7ab49117>
2026-02-26 14:32:08 +0000

    🧬 Gen 6706 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,13 +1,18 @@
-import numpy as np
-
 class Brain:
-    def __init__(self, num_neurons, num_synapses):
-        self.neurons = np.random.rand(num_neurons)
-        self.synapses = np.random.rand(num_synapses)
-        self.weights = np.random.rand(num_neurons, num_synapses)
+    def __init__(self):
+        self.weights = [[0.0 for _ in range(10)] for _ in range(10)]
+        self.biases = [0.0 for _ in range(10)]
+        self.neurons = [[0.0 for _ in range(10)] for _ in range(10)]
 
-    def fire(self, input_signals):
-        outputs = np.dot(input_signals, self.weights)
-        self.neurons += outputs
-        self.synapses *= self.synapses
-        return self.neurons
+    def process(self, input_vector):
+        for i in range(10):
+            self.neurons[0][i] = input_vector[i]
+        for i in range(1, 10):
+            for j in range(10):
+                self.neurons[i][j] = self.sigmoid(
+                    sum(x * self.weights[i - 1][j] for x in self.neurons[i - 1]) + self.biases[i - 1]
+                )
+        return self.neurons[-1]
+
+    def sigmoid(self, x):
+        return 1.0 / (1.0 + exp(-x))
