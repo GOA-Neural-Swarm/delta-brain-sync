e4dcd02ca0f9b6af7c9ef17b0913f694e56c326e
root <root@6570c282e0ab>
2026-02-26 16:41:41 +0000

    🧬 Gen 6769 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,18 +1,9 @@
+import numpy as np
+
 class Brain:
-    def __init__(self):
-        self.neurons = []
-        self.connections = {}
+    def __init__(self, num_neurons):
+        self.num_neurons = num_neurons
+        self.weights = np.random.rand(num_neurons, num_neurons)
 
-    def add_neuron(self, neuron):
-        self.neurons.append(neuron)
-        self.connections[neuron.id] = []
-
-    def connect_neurons(self, neuron1, neuron2, weight):
-        self.connections[neuron1.id].append((neuron2, weight))
-        self.connections[neuron2.id].append((neuron1, weight))
-
-    def process(self):
-        for neuron in self.neurons:
-            neuron.update()
-            for connected_neuron, weight in self.connections[neuron.id]:
-                neuron.update_with_input(connected_neuron.output * weight)
+    def process(self, inputs):
+        return np.dot(inputs, self.weights)
