40516d636a328e1460562deeb6706571607e0975
root <root@2e7b7ab49117>
2026-02-26 14:43:17 +0000

    🧬 Gen 6712 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,37 +1,39 @@
-import time
-
-class OptimizedBrain(Brain):
+class Brain:
     def __init__(self):
-        super().__init__()
-        self.neuron_outputs = [None] * len(self.neurons)
+        self.weights = {}
+        self.biases = {}
 
-    def propagate(self):
-        for i, neuron in enumerate(self.neurons):
-            if neuron['input_connections']:
-                neuron['output'] = neuron['activation_function'](
-                    sum(connection['weight'] * connection['input'] for connection in neuron['input_connections'])
-                )
-            else:
-                neuron['output'] = neuron['activation_function'](0)
-            self.neuron_outputs[i] = neuron['output']
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-    def get_output(self):
-        return self.neuron_outputs
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
 
-class BrainThread:
-    def __init__(self, brain):
-        self.brain = brain
-        self.thread_id = None
-        self.last_propagation_time = time.time()
+    def train(self, inputs, targets):
+        learning_rate = 0.1
+        for epoch in range(1000):
+            for i in range(len(inputs)):
+                input_layer = inputs[i]
+                target = targets[i]
+                predictions = self.forward_pass(input_layer)
+                error = target - predictions
+                self.backward_pass(input_layer, error, learning_rate)
+        return self
 
-    def propagate(self):
-        self.brain.propagate()
-        self.last_propagation_time = time.time()
+    def forward_pass(self, input_layer):
+        layer_outputs = {}
+        layer_outputs[0] = input_layer
+        for i in range(1, len(self.weights) + 1):
+            layer_outputs[i] = np.dot(layer_outputs[i - 1], self.weights[i - 1]) + self.biases[i - 1]
+            layer_outputs[i] = self.sigmoid(layer_outputs[i])
+        return layer_outputs[len(self.weights)]
 
-    def get_output(self):
-        return self.brain.get_output()
-
-brain = OptimizedBrain()
-brain_thread = BrainThread(brain)
-brain_thread.propagate()
-print(brain_thread.get_output())
+    def backward_pass(self, input_layer, error, learning_rate):
+        layer_outputs = {}
+        layer_outputs[0] = input_layer
+        for i in range(len(self.weights), 0, -1):
+            error = error * self.sigmoid_derivative(layer_outputs[i])
+            self.weights[i - 1] += learning_rate * np.dot(layer_outputs[i - 1].T, error)
+            self.biases[i - 1] += learning_rate * error
+            error = np.dot(error, self.weights[i - 1].T)
+        return self
