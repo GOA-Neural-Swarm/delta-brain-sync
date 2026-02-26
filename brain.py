10df30fbdfe37626f93c5d2d68f67909151aa89d
root <root@6570c282e0ab>
2026-02-26 15:05:07 +0000

    🧬 Gen 6721 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,21 +1,4 @@
-import numpy as np
-from numba import jit
-
-class Brain:
-    def __init__(self):
-        self.synapses = {}
-
-    @jit(nopython=True)
-    def process(self, inputs):
-        outputs = np.zeros((len(inputs),))
-        for i, input in enumerate(inputs):
-            if i in self.synapses:
-                outputs[i] = self.synapses[i] * input
-            else:
-                outputs[i] = input
-        return outputs
-
-    def train(self, inputs, outputs):
-        for i, input in enumerate(inputs):
-            if i not in self.synapses:
-                self.synapses[i] = np.mean((outputs[i] - input) / np.std((outputs[i] - input)))
+from numba import njit
+@njit
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
