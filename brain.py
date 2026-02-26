2343369ee9f5476240787041ad2fee5fb942fa88
root <root@6570c282e0ab>
2026-02-26 19:22:17 +0000

    🧬 Gen 6846 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,35 +1,27 @@
-import numpy as np
+import time
+import random
 
-class Brain:
+class DistributedBrain(Brain):
     def __init__(self):
-        self.weights = []
-        self.biases = []
-        self.layers = []
+        super().__init__()
+        self._servers = []
 
-    def add_layer(self, neurons, activation):
-        self.layers.append((neurons, activation))
-        self.weights.append(np.random.rand(neurons, sum([n for n, _ in self.layers[:-1]])))
-        self.biases.append(np.zeros((neurons,)))
+    def _run(self, neuron):
+        output = [n for n in self.neurons[neuron]['output']]
+        return output
 
-    def forward_pass(self, inputs):
-        outputs = np.copy(inputs)
-        for i, (neurons, activation) in enumerate(self.layers):
-            weights = self.weights[i]
-            biases = self.biases[i]
-            if i == 0:
-                outputs = np.dot(weights, outputs) + biases
-            else:
-                outputs = activation(outputs)
-        return outputs
+    def run(self):
+        start_time = time.time()
+        with distributed.Client() as client:
+            self._servers = client.submit(self._run, list(self.neurons.keys()))
+            results = client.gather(self._servers)
+        end_time = time.time()
+        print(f"Run time: {end_time - start_time:.6f} seconds")
+        return [n for n in self.neurons.values() if n['output']]
 
-    def backpropagation(self, inputs, targets):
-        outputs = self.forward_pass(inputs)
-        errors = np.array([(target - output) * output * (1 - output) for target, output in zip(targets, outputs)])
-        for i in range(len(self.layers) - 1, -1, -1):
-            if i == len(self.layers) - 1:
-                errors = np.dot(errors, np.transpose(self.weights[i]))
-            else:
-                weights = self.weights[i]
-                outputs = np.dot(weights, outputs) + self.biases[i]
-                errors = np.dot(errors, np.transpose(weights)) * [f(x) for x, f in zip(outputs, [f for _, f in self.layers[i + 1]])]
-        return errors
+import distributed
+brain = DistributedBrain()
+brain.add_neuron('A')
+brain.add_neuron('B')
+brain.connect_neurons('A', 'B')
+brain.run()
