1a75ef0ca996823539859f7db81eb883ce201c44
root <root@6570c282e0ab>
2026-02-26 16:59:46 +0000

    🧬 Gen 6778 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,25 +1,10 @@
 import numpy as np
+import numba
 
+@numba.jit(nopython=True)
 class Brain:
     def __init__(self):
-        self.weights = np.random.rand(1000, 1000)
-        self.biases = np.random.rand(1000, 1)
-        self.output = np.zeros((1000, 1))
+        self.synaptic_weights = np.random.rand(10, 10)
 
-    def forward_pass(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-        return self.output
-
-    def backpropagation(self, inputs, targets):
-        output = self.forward_pass(inputs)
-        error = np.sum((output - targets) ** 2) / 2
-        delta_output = (output - targets)
-        delta_weights = np.dot(delta_output.T, inputs)
-        delta_biases = np.sum(delta_output, axis=0, keepdims=True)
-        return error, delta_weights, delta_biases
-
-    def train(self, inputs, targets, learning_rate=0.01):
-        error, delta_weights, delta_biases = self.backpropagation(inputs, targets)
-        self.weights -= learning_rate * delta_weights
-        self.biases -= learning_rate * delta_biases
-        return error
+    def think(self, inputs):
+        return np.dot(inputs, self.synaptic_weights)
