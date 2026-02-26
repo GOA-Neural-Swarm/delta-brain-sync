59216a6f4d47db636cf7169825b72904f2ec71f1
root <root@6570c282e0ab>
2026-02-26 16:19:19 +0000

    🧬 Gen 6758 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,27 +1,36 @@
-class Neuron:
-    def __init__(self, id):
-        self.id = id
-        self.output = 0
-        self.inputs = []
+import numpy as np
 
-    def add_input(self, input_neuron):
-        self.inputs.append(input_neuron)
+class Brain:
+    def __init__(self, inputs, outputs, layers):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.layers = [np.zeros((i, o)) for i, o in zip([inputs]+[l[1] for l in layers]+[outputs], [outputs]+[l[2] for l in layers]+[inputs])]
+        self.weights = [[np.random.rand(i, o) for o in [l[1] for l in layers]+[outputs]] for i in [inputs]+[l[1] for l in layers]+[outputs]]
+        self.biases = [[np.zeros((i, 1)) for i in [l[1] for l in layers]+[outputs]] for _ in range(len(layers)+1)]
 
-    def update(self):
-        self.output = self.calculate_output()
-        for input_neuron in self.inputs:
-            input_neuron.add_output(self)
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
 
-    def calculate_output(self):
-        # Optimized calculation for high-speed neural processing
-        return sum(input_neuron.output for input_neuron in self.inputs)
+    def sigmoid_derivative(self, x):
+        return x*(1-x)
 
-class Connection:
-    def __init__(self, neuron1, neuron2):
-        self.neuron1 = neuron1
-        self.neuron2 = neuron2
-        self.weight = 1
+    def train(self, inputs, outputs, iterations):
+        for _ in range(iterations):
+            layer_inputs = [np.append(self.layers[0], inputs)]
+            for i, (in_layer, out_layer, _) in enumerate(self.layers[1:]):
+                layer_inputs.append(np.dot(in_layer, self.weights[i+1].T)+self.biases[i+1])
+                layer_inputs[i+1] = self.sigmoid(layer_inputs[i+1])
+            error = np.sum((layer_inputs[-1]-outputs)**2)
+            dweights = [np.dot(layer_inputs[i+1].T, layer_inputs[i+1]-outputs)]
+            for i, (in_layer, out_layer, _) in enumerate(self.layers[:-1][::-1]):
+                dweights[i] = np.dot(dweights[i+1], out_layer) + dweights[i]
+                layer_inputs[i+1] = self.sigmoid_derivative(layer_inputs[i+1])
+            self.weights = [w - 0.1 * dw for w, dw in zip(self.weights, dweights)]
+            self.biases = [b - 0.1 * np.sum((dw > 0) * (dw < 1), axis=0, keepdims=True) for b, dw in zip(self.biases, dweights)]
 
-    def propagate(self):
-        self.neuron2.add_input(self.neuron1)
-        self.neuron1.output *= self.weight
+    def think(self, inputs):
+        layer_inputs = [np.append(self.layers[0], inputs)]
+        for i, (in_layer, out_layer, _) in enumerate(self.layers[1:]):
+            layer_inputs.append(np.dot(in_layer, self.weights[i+1].T)+self.biases[i+1])
+            layer_inputs[i+1] = self.sigmoid(layer_inputs[i+1])
+        return layer_inputs[-1]
