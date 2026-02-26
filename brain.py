48479883255fad4432048ac61cf0c9f84fa79a8b
root <root@6570c282e0ab>
2026-02-26 16:37:36 +0000

    🧬 Gen 6767 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,3 +1,18 @@
-class Neuron:
+class Brain:
     def __init__(self):
-        self.bias = 0
+        self.neurons = []
+        self.connections = {}
+
+    def add_neuron(self, neuron):
+        self.neurons.append(neuron)
+        self.connections[neuron.id] = []
+
+    def connect_neurons(self, neuron1, neuron2, weight):
+        self.connections[neuron1.id].append((neuron2, weight))
+        self.connections[neuron2.id].append((neuron1, weight))
+
+    def process(self):
+        for neuron in self.neurons:
+            neuron.update()
+            for connected_neuron, weight in self.connections[neuron.id]:
+                neuron.update_with_input(connected_neuron.output * weight)
