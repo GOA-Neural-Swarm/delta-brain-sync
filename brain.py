f6bb61c955660bb6a2bf9346fcb28ccda298de53
root <root@2e7b7ab49117>
2026-02-26 13:40:56 +0000

    🧬 Gen 6678 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,31 +1,16 @@
-from collections import defaultdict
-from numba import jit, types
-import numpy as np
-
 class Brain:
-    def __init__(self, neurons, synapses):
-        self.neurons = neurons
-        self.synapses = synapses
-        self.cache = defaultdict(int)
-        self.cache_hits = 0
-        self.cache_misses = 0
+    def __init__(self):
+        self.neurons = {}
 
-    @jit
-    def process(self, inputs):
-        if inputs in self.cache:
-            self.cache_hits += 1
-            return self.cache[inputs]
+    def add_neuron(self, name, value):
+        self.neurons[name] = value
+
+    def fire_synapse(self, source, target):
+        if source in self.neurons and target in self.neurons:
+            self.neurons[target] += self.neurons[source]
         else:
-            self.cache_misses += 1
-            output = self._calculate_output(inputs)
-            self.cache[inputs] = output
-            return output
+            print("Invalid neuron names")
 
-    @jit
-    def _calculate_output(self, inputs):
-        output = np.zeros(len(self.neurons))
-        for i, neuron in enumerate(self.neurons):
-            for synapse in self.synapses:
-                if synapse['input'] == i:
-                    output[i] += neuron * synapse['weight']
-        return output
+    def update_neurons(self):
+        for neuron in self.neurons:
+            self.neurons[neuron] *= 0.95
