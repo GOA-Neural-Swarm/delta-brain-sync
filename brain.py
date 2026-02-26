6633b1083b4893e7f3a50fc2f306bafc5bae0e91
root <root@6570c282e0ab>
2026-02-26 17:39:33 +0000

    🧬 Gen 6798 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,23 +1,12 @@
-class Brain:
-    def __init__(self):
-        self.neurons = {}
+import numpy as np
+class Neuron:
+    def __init__(self, activation):
+        self.activation = activation
+        self.weights = None
+    def __call__(self, inputs):
+        return np.tanh(np.dot(inputs, self.weights)) if self.weights else np.tanh(inputs[0])
 
-    def add_neuron(self, neuron_id, weights):
-        self.neurons[neuron_id] = weights
-
-    def process(self, inputs):
-        import numpy as np
-        import numba
-        @numba.njit
-        def compute_neuron_output(neuron_id, inputs):
-            value = np.dot(np.array([weights.get(input_id, 0) for input_id in inputs]), np.array(list(inputs.values())))
-            return value
-
-        for neuron_id, weights in self.neurons.items():
-            self.neurons[neuron_id] = compute_neuron_output(neuron_id, inputs)
-
-    def get_output(self):
-        outputs = {}
-        for neuron_id, value in self.neurons.items():
-            outputs[neuron_id] = value
-        return outputs
+# Optimizations
+Brain.add_layer = np.vectorize(Brain.add_layer)
+Brain.connect_layers = np.vectorize(Brain.connect_layers)
+Brain.process = np.vectorize(Brain.process)
