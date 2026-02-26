5ef14e2eae18bbf445c1ee6397fc622c7ec7d0aa
root <root@2e7b7ab49117>
2026-02-26 13:56:15 +0000

    🧬 Gen 6686 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -2,26 +2,21 @@ import numpy as np
 
 class Brain:
     def __init__(self):
-        self.weights = np.random.rand(100, 100)  # Initialize weights with random values
-        self.biases = np.random.rand(100)  # Initialize biases with random values
+        self.synaptic_weights = np.random.rand(4, 4)
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
+    def think(self, inputs):
+        return np.dot(inputs, self.synaptic_weights)
 
     def train(self, inputs, outputs):
-        # Forward pass
-        outputs_pred = self.forward_pass(inputs)
-        error = np.mean((outputs_pred - outputs) ** 2)
+        self.synaptic_weights += np.dot(inputs.T, outputs - self.think(inputs)) / inputs.shape[0]
 
-        # Backward pass
-        deltas = 2 * (outputs_pred - outputs)
-        hidden_layer_error = np.dot(deltas, self.weights.T)
-        self.weights += np.dot(inputs.T, hidden_layer_error)
-        self.biases += np.sum(hidden_layer_error, axis=0)
+    def mutate(self):
+        self.synaptic_weights += np.random.normal(0, 0.1, size=self.synaptic_weights.shape)
 
-    def forward_pass(self, inputs):
-        hidden_layer = np.dot(inputs, self.weights) + self.biases
-        return self.sigmoid(hidden_layer)
+    def crossover(self, other):
+        mask = np.random.rand(*self.synaptic_weights.shape) < 0.5
+        self.synaptic_weights[mask] = other.synaptic_weights[mask]
+
+    def selection(self, brains):
+        scores = [brain.think(inputs) for brain in brains]
+        return brains[np.argmax(scores)]
