94317c1044a5494799e47cea76bf525c1188caa0
root <root@6570c282e0ab>
2026-02-26 16:31:30 +0000

    🧬 Gen 6764 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,16 +1,10 @@
 import numpy as np
 
 class Brain:
-    def __init__(self, num_inputs, num_hidden, num_outputs):
-        self.num_inputs = num_inputs
-        self.num_hidden = num_hidden
-        self.num_outputs = num_outputs
-
-        self.weights_ih = np.random.rand(num_inputs, num_hidden)
-        self.weights_ho = np.random.rand(num_hidden, num_outputs)
-
-        self.bias_h = np.zeros((num_hidden,))
-        self.bias_o = np.zeros((num_outputs,))
+    def __init__(self):
+        self.weights = np.random.rand(100, 100)
+        self.biases = np.random.rand(100, 1)
+        self.output = None
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -18,17 +12,18 @@ class Brain:
     def sigmoid_derivative(self, x):
         return x * (1 - x)
 
-    def train(self, inputs, targets):
-        outputs = self.predict(inputs)
-        errors = targets - outputs
-        self.weights_ho += np.dot(inputs.T, errors)
-        self.weights_ih += np.dot(inputs.T, np.dot(self.sigmoid(np.dot(inputs, self.weights_ih) + self.bias_h), self.weights_ho.T) * errors)
-        self.bias_h += np.dot(np.ones((1, inputs.shape[1])), np.dot(self.sigmoid(np.dot(inputs, self.weights_ih) + self.bias_h), self.weights_ho.T) * errors)
-        self.bias_o += np.dot(np.ones((1, outputs.shape[1])), np.dot(self.sigmoid(np.dot(inputs, self.weights_ih) + self.bias_h), self.weights_ho.T) * errors)
+    def forward_pass(self, inputs):
+        self.inputs = inputs
+        self.output = self.sigmoid(np.dot(self.weights, inputs) + self.biases)
 
-    def predict(self, inputs):
-        hidden_layer_activation = np.dot(inputs, self.weights_ih) + self.bias_h
-        hidden_layer_output = self.sigmoid(hidden_layer_activation)
-        output_layer_activation = np.dot(hidden_layer_output, self.weights_ho) + self.bias_o
-        output_layer_output = self.sigmoid(output_layer_activation)
-        return output_layer_output
+    def backward_pass(self, targets):
+        self.targets = targets
+        error = np.mean((self.output - self.targets) ** 2)
+        d_output = 2 * (self.output - self.targets)
+        d_weights = np.dot(d_output, self.inputs.T)
+        d_biases = d_output
+        return error, d_weights, d_biases
+
+    def update_weights(self, error, d_weights, d_biases, learning_rate):
+        self.weights -= learning_rate * d_weights
+        self.biases -= learning_rate * d_biases
