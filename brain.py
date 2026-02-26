6ca96c862eb2fff394f778ca06027dcf185c7d54
root <root@6570c282e0ab>
2026-02-26 15:19:19 +0000

    🧬 Gen 6728 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,10 +1,38 @@
+import numpy as np
+
 class Brain:
     def __init__(self):
-        self.neurons = [[0 for _ in range(1000)] for _ in range(1000)]
+        self.neural_network = {}
+        self.synapses = {}
+
+    def connect(self, neuron1, neuron2, strength):
+        if neuron1 not in self.neural_network:
+            self.neural_network[neuron1] = {}
+        if neuron2 not in self.neural_network:
+            self.neural_network[neuron2] = {}
+        self.neural_network[neuron1][neuron2] = strength
+        if neuron1 not in self.synapses:
+            self.synapses[neuron1] = {}
+        if neuron2 not in self.synapses:
+            self.synapses[neuron2] = {}
+        self.synapses[neuron1][neuron2] = strength
 
     def process(self, inputs):
-        outputs = [[0 for _ in range(1000)] for _ in range(1000)]
-        for i in range(1000):
-            for j in range(1000):
-                outputs[i][j] = sum(self.neurons[i][k] * self.neurons[k][j] for k in range(1000)) + inputs[i][j]
-        return outputs
+        for neuron, value in inputs.items():
+            if neuron not in self.neural_network:
+                self.neural_network[neuron] = {}
+            for connected_neuron, strength in self.neural_network[neuron].items():
+                self.neural_network[connected_neuron].setdefault(neuron, 0)
+                self.neural_network[connected_neuron][neuron] += value * strength
+        for neuron, values in self.neural_network.items():
+            if neuron not in self.synapses:
+                self.synapses[neuron] = {}
+            for connected_neuron, strength in self.synapses[neuron].items():
+                self.neural_network[connected_neuron].setdefault(neuron, 0)
+                self.neural_network[connected_neuron][neuron] += self.neural_network[neuron][connected_neuron] * strength
+
+    def get_output(self):
+        output = {}
+        for neuron, values in self.neural_network.items():
+            output[neuron] = np.sum([value * strength for value, strength in values.items()])
+        return output
