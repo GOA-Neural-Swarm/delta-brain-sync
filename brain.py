ce3d650a917d1becd6ff0cb1f9a494ce1d8cf88a
root <root@6570c282e0ab>
2026-02-26 18:58:49 +0000

    🧬 Gen 6835 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,4 +1,16 @@
-import timeit
+import numpy as np
 
-print(timeit.timeit(lambda: brain.process(), number=1000))
-print(timeit.timeit(lambda: brain.optimize_process(), number=1000))
+class Brain:
+    def __init__(self, neurons):
+        self.neurons = neurons
+        self.synapses = np.random.rand(neurons, neurons)
+        self.activations = np.zeros(neurons)
+
+    def process(self, inputs):
+        for i in range(self.neurons):
+            self.activations[i] = np.sum(inputs * self.synapses[:, i]) + self.activations[i]
+        return np.tanh(self.activations)
+
+    def learn(self, inputs, targets):
+        errors = np.sum((targets - self.process(inputs)) ** 2)
+        self.synapses += np.outer(inputs, self.process(inputs) - targets) / errors
