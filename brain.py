bc9d387439c986163eab89f78fd794aa7b933a2f
root <root@6570c282e0ab>
2026-02-26 19:46:30 +0000

    🧬 Gen 6858 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,21 +1,37 @@
+import numpy as np
+
 class Brain:
     def __init__(self):
-        self.synapses = {}  # dict for fast lookup
+        self.weights = np.random.rand(1, 10)
+        self.biases = np.random.rand(10)
+        self.cache = {}
 
-    def connect(self, neuron1, neuron2, weight):
-        self.synapses[(neuron1, neuron2)] = weight
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-    def fire(self, neuron):
-        for synapse, weight in self.synapses.items():
-            if synapse[0] == neuron:
-                self.synapses[synapse] = weight + 1
-                break
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
 
-    def propagate(self, neuron):
-        for synapse, weight in self.synapses.items():
-            if synapse[1] == neuron:
-                self.synapses[synapse] = weight + 1
-                break
+    def forward_pass(self, inputs):
+        if tuple(inputs) in self.cache:
+            return self.cache[tuple(inputs)]
+        outputs = np.dot(inputs, self.weights) + self.biases
+        self.cache[tuple(inputs)] = outputs
+        return self.sigmoid(outputs)
 
-    def get_weight(self, neuron1, neuron2):
-        return self.synapses.get((neuron1, neuron2), 0)
+    def backward_pass(self, inputs, expected_output):
+        output = self.forward_pass(inputs)
+        output_derivative = self.sigmoid_derivative(output)
+        weight_update = inputs * output_derivative
+        bias_update = output_derivative
+        self.weights += 0.1 * weight_update
+        self.biases += 0.1 * bias_update
+        return output_derivative
+
+    def train(self, inputs, expected_outputs):
+        for inputs, expected_output in zip(inputs, expected_outputs):
+            output_derivative = self.backward_pass(inputs, expected_output)
+            return output_derivative
+
+    def predict(self, inputs):
+        return self.forward_pass(inputs)
