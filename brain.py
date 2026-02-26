67f847bf99aaaa07c44b6077683455893416b43b
root <root@6570c282e0ab>
2026-02-26 19:27:40 +0000

    🧬 Gen 6849 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,10 +1,17 @@
 import numpy as np
 
 class Brain:
-    def __init__(self, num_neurons, num_synapses):
-        self.num_neurons = num_neurons
-        self.num_synapses = num_synapses
-        self.synaptic_weights = np.random.rand(num_synapses).flatten()
+    def __init__(self):
+        self.neurons = np.zeros((1000, 1000), dtype=int)
 
-    def process(self, input_values):
-        return np.dot(input_values, self.synaptic_weights)
+    def propagate(self, input_vector):
+        output_vector = np.zeros((1000,), dtype=int)
+        for i in range(1000):
+            for j in range(1000):
+                output_vector[i] += self.neurons[i, j] * input_vector[j]
+        return output_vector
+
+    def train(self, input_vector, target_vector):
+        output_vector = self.propagate(input_vector)
+        error = np.sum((output_vector - target_vector) ** 2)
+        self.neurons += error * np.outer(input_vector, target_vector)
