a30249ecfbf1d3f25a2d21d1c605fa63b221e8f6
root <root@6570c282e0ab>
2026-02-26 17:23:53 +0000

    🧬 Gen 6790 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,12 +1,1 @@
-def main():
-    brain = Brain()
-    brain.add_layer(Layer(np.array([[1, 2]]), np.array([1])))
-    brain.add_optimizer(Optimizer(0.01))
-
-    inputs = np.array([[1, 2], [3, 4]])
-    targets = np.array([1, 2])
-
-    brain.train(inputs, targets)
-
-if __name__ == "__main__":
-    main()
+import numpy as np
