adcd7190ad4eb355d950c6350e6b2d0bd98ff3ad
root <root@2e7b7ab49117>
2026-02-26 14:19:05 +0000

    🧬 Gen 6698 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,17 +1,22 @@
 import numpy as np
 
 class Brain:
-    def __init__(self):
-        self.weights = np.random.rand(1000, 1000)
-        self.biases = np.random.rand(1000)
+    def __init__(self, num_inputs, num_hidden, num_outputs):
+        self.num_inputs = num_inputs
+        self.num_hidden = num_hidden
+        self.num_outputs = num_outputs
+        self.weights1 = np.random.rand(num_inputs, num_hidden)
+        self.weights2 = np.random.rand(num_hidden, num_outputs)
 
-    def forward_pass(self, inputs):
-        hidden_layer = np.dot(inputs, self.weights) + self.biases
-        output_layer = np.maximum(hidden_layer, 0)
-        return output_layer
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-    def backward_pass(self, inputs, targets):
-        hidden_layer = self.forward_pass(inputs)
-        output_layer = hidden_layer
-        error = targets - output_layer
-        return error
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def predict(self, inputs):
+        hidden_layer_output = np.dot(inputs, self.weights1)
+        hidden_layer_output = self.sigmoid(hidden_layer_output)
+        output_layer_output = np.dot(hidden_layer_output, self.weights2)
+        output_layer_output = self.sigmoid(output_layer_output)
+        return output_layer_output
