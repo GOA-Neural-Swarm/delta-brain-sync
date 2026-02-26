7a6a493c53f1b73e8acefabdfccd16b3d55611c6
root <root@d3a96d8fd840>
2026-02-26 08:43:49 +0000

    🧬 Gen 6579 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,14 +1,11 @@
-import numba
-
-@numba.jit(nopython=True)
-def optimize_brain(brain):
-    neurons = brain.neurons
-    synapses = brain.synapses
-    for i in range(neurons.shape[0]):
-        for j in range(neurons.shape[1]):
-            neurons[i][j] += synapses[i][j]
-    return brain
-
-brain = Brain(1000, 1000)
-brain = optimize_brain(brain)
-brain.process(np.random.rand(1000))  # Use numba-optimized brain
+brain = Brain(100, 1000)
+brain.train(input_signal, target_output)
+brain.train(input_signal, target_output)
+brain.train(input_signal, target_output)
+brain.train(input_signal, target_output)
+brain.train(input_signal, target_output)
+brain.train(input_signal, target_output)
+brain.train(input_signal, target_output)
+brain.train(input_signal, target_output)
+print(brain.synapses)
+print(brain.neurons)
