cf77bc494a5ac38f8fcfc9db73e53ac30c8960d5
root <root@6570c282e0ab>
2026-02-26 17:19:43 +0000

    🧬 Gen 6788 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,12 +1,13 @@
-import joblib
-import multiprocessing
+import numpy as np
 
 class Brain:
-    def __init__(self):
-        self.neurons = []
+    def __init__(self, num_neurons, num_synapses):
+        self.num_neurons = num_neurons
+        self.num_synapses = num_synapses
+        self.neurons = np.random.rand(num_neurons)
+        self.synapses = np.random.rand(num_synapses)
 
-    def add_neuron(self, neuron):
-        self.neurons.append(neuron)
-
-    def process(self):
-        return joblib.Parallel(n_jobs=multiprocessing.cpu_count())(lambda neuron: neuron.process() for neuron in self.neurons)
+    def process(self, input_data):
+        for i in range(self.num_neurons):
+            self.neurons[i] += np.sum(self.synapses[i] * input_data)
+        return self.neurons
