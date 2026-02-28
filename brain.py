ecb4c15751c4e8fde2c193cef64b40394cd79849
root <root@7e91cbe80942>
2026-02-28 01:34:29 +0000

    🧬 Gen 6900 Hyper-Evolution [skip ci]

--- brain.py
+++ brain.py
@@ -1,37 +1,30 @@
+import numpy as np
+
 class Brain:
     def __init__(self):
-        self.neurons = {}
+        self.neurons = np.zeros(1000000)
         self.connections = {}
         self.weights = {}
 
-    def connect(self, neuron1, neuron2, weight):
-        if neuron1 not in self.neurons:
-            self.neurons[neuron1] = []
-        if neuron2 not in self.neurons:
-            self.neurons[neuron2] = []
-        self.connections[(neuron1, neuron2)] = weight
-        self.neurons[neuron1].append(neuron2)
-        self.neurons[neuron2].append(neuron1)
+    def add_neuron(self):
+        neuron_id = len(self.neurons)
+        self.neurons[neuron_id] = 0
+        return neuron_id
 
-    def fire(self, neuron):
-        if neuron not in self.neurons:
-            return
-        for connected_neuron in self.neurons[neuron]:
-            if (neuron, connected_neuron) in self.connections:
-                weight = self.connections[(neuron, connected_neuron)]
-                self.weights[(neuron, connected_neuron)] = weight
-                for neighbor in self.neurons[connected_neuron]:
-                    if neighbor!= neuron:
-                        if (neighbor, connected_neuron) in self.connections:
-                            weight = self.connections[(neighbor, connected_neuron)]
-                            self.weights[(neighbor, connected_neuron)] = weight
-                        if (connected_neuron, neighbor) in self.connections:
-                            weight = self.connections[(connected_neuron, neighbor)]
-                            self.weights[(connected_neuron, neighbor)] = weight
+    def add_connection(self, neuron1, neuron2, weight):
+        if neuron1 not in self.connections:
+            self.connections[neuron1] = {}
+        if neuron2 not in self.connections:
+            self.connections[neuron2] = {}
+        self.connections[neuron1][neuron2] = weight
+        self.connections[neuron2][neuron1] = weight
+        self.weights[neuron1, neuron2] = weight
 
-    def optimize(self):
-        for neuron in self.neurons:
-            for connected_neuron in self.neurons[neuron]:
-                if (neuron, connected_neuron) in self.connections:
-                    weight = self.connections[(neuron, connected_neuron)]
-                    self.connections[(neuron, connected_neuron)] = weight * 0.9
+    def fire(self, neuron_id):
+        neuron_id %= len(self.neurons)
+        self.neurons[neuron_id] = 1
+        for connection in self.connections.get(neuron_id, []):
+            self.neurons[connection] = 1
+
+    def get_output(self):
+        return np.where(self.neurons == 1)[0].tolist()
